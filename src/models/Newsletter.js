9b45d76e991181b2c2cfae8350777e3df39dba21
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-17 20:55:41 +0530

    added keywords for newsletters

--- src/models/Newsletter.js
+++ src/models/Newsletter.js
@@ -12,12 +12,13 @@ mongoose.connect(process.env.MONGO_URL, {
  * reference
  * emailSubject
  * body
- * status
+ * status [ draft, published, deleted ]
  * createdAt
  * creator
  * lastSavedAt
  * sentAt
  * recipients
+ * keywords
  *
  */
 
@@ -31,6 +32,7 @@ const newsletterSchema = mongoose.Schema({
   lastSavedAt: Date,
   sentAt: Date,
   recipients: [mongoose.ObjectId],
+  keywords: [String],
 });
 
 module.exports = mongoose.model("Newsletter", newsletterSchema);
