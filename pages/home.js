aeecd0ff927aec3ea971830b3ed2e4740a557195
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-17 16:12:29 +0530

    removed key warning

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,16 @@
-import Card from '../components/Card/Card'
-import { getCardsData } from '../helpers/cardsFetcher'
+import Card from "../components/Card/Card";
+import { getCardsData } from "../helpers/cardsFetcher";
 
 const HomePage = () => {
-  const cards = getCardsData()
+  const cards = getCardsData();
 
   return (
     <>
-      {cards?.map((card) => (
-        <Card title={card.title} subtitle={card.total} />
+      {cards?.map((card, i) => (
+        <Card key={i} title={card.title} subtitle={card.total} />
       ))}
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
