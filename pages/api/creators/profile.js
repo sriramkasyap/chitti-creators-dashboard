11c9a67969dde86bedbdc2381f5a813dbbc3127c
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-18 21:15:14 +0530

    update profile endpoint added

---
+++ pages/api/creators/profile.js
@@ -0,0 +1,47 @@
+import Creator from "../../../src/models/Creator";
+
+// Update Creator profile
+export default async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const creatorId = req.body.id;
+      // After auth, the creator ID comes here
+
+      const { profile } = req.body;
+      if (!profile) throw new Error("Invalid request");
+
+      delete profile.emailId;
+      delete profile._id;
+
+      var creator = await Creator.findById(creatorId);
+
+      if (!creator) throw new Error("Creator does not exist");
+
+      var result = await Creator.findByIdAndUpdate(
+        creatorId,
+        {
+          $set: { ...profile },
+        },
+        {
+          new: true,
+        }
+      );
+
+      res.send({
+        success: true,
+        result,
+      });
+    } catch (error) {
+      console.error(error);
+      res.send({
+        error: true,
+        message: error.message,
+      });
+    }
+  } else {
+    re.status(404).send({
+      error: true,
+      message: "Invalid Request",
+    });
+  }
+};
