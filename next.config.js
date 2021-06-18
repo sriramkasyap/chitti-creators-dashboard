64ae5dac55ad698c496b3dac26e87e9603b9d6ea
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-18 22:04:36 +0530

    revamped next config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,13 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
-  var common = {
+  return {
     env: {
       MONGO_DB: "chitti",
+      MONGO_URL:
+        phase === PHASE_DEVELOPMENT_SERVER
+          ? "mongodb://localhost:27017/"
+          : "mongodb+srv://staging_user:OPOR6NAMa4OkKcgm@devcluster.pjhyg.mongodb.net/chitti?retryWrites=true&w=majority",
     },
   };
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    var devConfig = {
-      // MONGO_URL: "mongodb://localhost:27017/",
-      MONGO_URL:
-        "mongodb+srv://staging_user:OPOR6NAMa4OkKcgm>@devcluster.pjhyg.mongodb.net/chitti?retryWrites=true&w=majority",
-    };
-    return {
-      ...common,
-      ...devConfig,
-    };
-  } else {
-    var prodConfig = {
-      MONGO_URL:
-        "mongodb+srv://staging_user:OPOR6NAMa4OkKcgm>@devcluster.pjhyg.mongodb.net/chitti?retryWrites=true&w=majority",
-    };
-    return {
-      ...common,
-      ...prodConfig,
-    };
-  }
 };
