873ef67dbd63d830f0e0f65e3212965eb6880183
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-19 10:46:41 +0530

    Do not  commit monngo urls to repo

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     env: {
       MONGO_DB: "chitti",
-      MONGO_URL:
-        phase === PHASE_DEVELOPMENT_SERVER
-          ? "mongodb+srv://staging_user:OPOR6NAMa4OkKcgm@devcluster.pjhyg.mongodb.net/chitti?retryWrites=true&w=majority"
-          : "mongodb+srv://staging_user:OPOR6NAMa4OkKcgm@devcluster.pjhyg.mongodb.net/chitti?retryWrites=true&w=majority",
     },
   };
 };
