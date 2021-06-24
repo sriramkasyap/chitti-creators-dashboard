4f4749df0d5d8b6bcf2cf83ec5714a6e2d9de47f
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-25 01:16:18 +0530

    newsletters call added

--- pages/newsletters/index.js
+++ pages/newsletters/index.js
@@ -1,8 +1,45 @@
+import Link from "next/link";
 import { withIronSession } from "next-iron-session";
 import { Text } from "@chakra-ui/react";
 import { checkAuthentication, getIronConfig } from "../../src/utils";
+import { useEffect, useState } from "react";
+import { getNewsletters } from "../../src/helpers/userFetcher";
 
 const Newsletters = () => {
+  var [newsletters, setNewsletters] = useState([]); // Newsletters
+  var [loading, setLoading] = useState(true); // Loading State
+  var [error, setError] = useState(""); // Error message
+
+  useEffect(() => {
+    // Set newsletters on first load
+    getNewsletters()
+      .then((data) => {
+        if (data.success) {
+          setNewsletters(data.newsletters);
+          setLoading(false);
+        } else {
+          setError(data.message);
+          setLoading(false);
+        }
+      })
+      .catch((e) => {
+        setError(e.message);
+        setLoading(false);
+      });
+  }, []);
+
+  const getNewsletterLink = (newsletterId) => {
+    // Get link component for Editing newsletter
+    return (
+      <Link
+        href={`/newsletters/${newsletterId}`}
+        as={`/newsletters/[newsletterId]`}
+      >
+        Edit
+      </Link>
+    );
+  };
+
   return <Text fontSize="6xl">Newsletters Page</Text>;
 };
 
--- src/helpers/userFetcher.js
+++ src/helpers/userFetcher.js
@@ -1,11 +1,41 @@
-const DUMMY_USER = {
-  user_id: 95123,
-  first_name: 'Rushiraj',
-  last_name: 'Brahmbhatt',
-  category: 'Technology',
-  profile_image: 'media.png'
-}
+import { serializeObject } from "../utils";
 
-export const getUserData = () => {
-  return DUMMY_USER
-}
+export const getNewsletters = async (filters = {}) => {
+  return fetch(`/api/newsletters?${serializeObject(filters)}`).then((r) =>
+    r.json()
+  );
+};
+
+export const getSubscribers = async () => {
+  return fetch(`/api/subscribers`).then((r) => r.json());
+};
+
+export const updateProfile = async (profile) => {
+  return fetch("/api/creators/profile", {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ profile }),
+  }).then((r) => r.json());
+};
+
+export const addPlan = async (plan) => {
+  return fetch("/api/creators/plans", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(plan),
+  }).then((r) => r.json());
+};
+
+export const updatePlan = async (planId, plan) => {
+  return fetch(`/api/creators/plans/${planId}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(plan),
+  }).then((r) => r.json());
+};
