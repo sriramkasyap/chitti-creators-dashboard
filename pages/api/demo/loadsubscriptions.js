648e63ebe48b5a5c0c2500131534e8429869fc97
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-26 11:11:24 +0530

    chain the loading of subscriptions

--- pages/api/demo/loadsubscriptions.js
+++ pages/api/demo/loadsubscriptions.js
@@ -4,21 +4,24 @@ import SubscriptionPlan from "../../../src/models/SubscriptionPlan";
 export default async (req, res) => {
   var subscribers = await Subscriber.find();
   var plans = await SubscriptionPlan.find();
-  subscribers.forEach(async (user) => {
-    var plan = Math.floor(Math.random() * (plans.length - 1));
-    await Subscriber.findByIdAndUpdate(user._id, {
+  var subPromises = subscribers.map(async (user) => {
+    var plan = Math.floor(Math.random() * plans.length);
+    var r1 = await Subscriber.findByIdAndUpdate(user._id, {
       $push: {
         subscriptions: plans[plan]._id,
       },
     });
-    await SubscriptionPlan.findByIdAndUpdate(plans[plan]._id, {
+    var r2 = await SubscriptionPlan.findByIdAndUpdate(plans[plan]._id, {
       $push: {
         subscribers: user._id,
       },
     });
+
+    return [r1, r2];
   });
 
   res.send({
     success: true,
+    subPromises,
   });
 };
