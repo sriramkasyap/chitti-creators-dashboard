acec56d4eb8f06a82b61f6cb93d101d50998751c
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-28 11:39:21 +0530

    logo adjusted

--- src/components/common/Sidebar/WideScreenSidebar/Header/Header.js
+++ src/components/common/Sidebar/WideScreenSidebar/Header/Header.js
@@ -2,7 +2,7 @@ import { Heading } from "@chakra-ui/react";
 
 const Header = ({ title }) => {
   return (
-    <Heading mt={50} fontSize="6xl" alignSelf="center" letterSpacing="tight">
+    <Heading mt={3.5} fontSize="6xl" alignSelf="center" letterSpacing="tight">
       {title}
     </Heading>
   );
