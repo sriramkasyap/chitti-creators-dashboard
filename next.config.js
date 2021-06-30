863890a05a78d7f51e511b65901714d48c6a29a7
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-30 09:57:42 +0530

    display picture upload functionality added

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = (phase, { defaultConfig }) => {
     env: {
       MONGO_DB: "chitti",
       AUTH_COOKIE_NAME: "epsilon",
+      CLOUD_NAME: "chitti",
+      CLOUD_UPLOAD_PRESET: "chitti_dp",
     },
   };
 };
