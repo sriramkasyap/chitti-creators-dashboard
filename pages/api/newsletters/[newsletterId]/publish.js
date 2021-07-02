8a8b498a2a611a92b3b1b9e1a6fe5960df561b2c
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-07-02 14:42:27 +0530

    emails are sent to the creator

--- pages/api/newsletters/[newsletterId]/publish.js
+++ pages/api/newsletters/[newsletterId]/publish.js
@@ -1,8 +1,10 @@
 import { withIronSession } from "next-iron-session";
+import { sendMail } from "../../../../src/helpers/mailHelper";
 import withCreatorAuth from "../../../../src/middleware/withCreatorAuth";
+import Creator from "../../../../src/models/Creator";
 import Newsletter from "../../../../src/models/Newsletter";
 import SubscriptionPlan from "../../../../src/models/SubscriptionPlan";
-import { getIronConfig } from "../../../../src/utils";
+import { getIronConfig, tablifyEmailer } from "../../../../src/utils";
 
 export default withIronSession(
   withCreatorAuth(async (req, res) => {
@@ -10,7 +12,7 @@ export default withIronSession(
       if (req.method === "POST") {
         // Publish the newsleter and send
 
-        var { creatorId } = req.creator;
+        var { creatorId, emailId } = req.creator;
 
         var { newsletterId } = req.query;
         var { planId } = req.body;
@@ -29,14 +31,24 @@ export default withIronSession(
         var plan = await SubscriptionPlan.findById(planId);
         if (!plan) throw new Error("Plan does not exist");
 
+        var creator = await Creator.findById(creatorId);
+
         // Queue the newsletter to be sent to all the recipients
 
         newsletter.recipients = plan.subscribers;
         newsletter.status = "published";
         newsletter.lastSavedAt = Date.now();
         newsletter.sentAt = Date.now();
+        var tabled = tablifyEmailer(newsletter.body);
 
-        newsletter.save();
+        await sendMail(
+          creator.emailId,
+          creator.fullName,
+          newsletter.emailSubject,
+          tabled
+        );
+
+        await newsletter.save();
 
         return res.send({
           success: true,
---
+++ src/helpers/mailHelper.js
@@ -0,0 +1,20 @@
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_DASHBOARD_KEY);
+
+export const sendMail = async (to, fromName, subject, html) => {
+  const msg = {
+    to, // Mail recipient
+    from: `${fromName} <chitti.epsilon@gmail.com>`, // Verified Sender
+    subject,
+    html,
+  };
+  return sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
