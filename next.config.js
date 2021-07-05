8f34afa22540618cf8a5a4481607ecc285226401
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-07-05 19:20:02 +0530

    Sentry integrated for error logging and monitoring

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-module.exports = () => {
-  return {
-    env: {
-      MONGO_DB: "chitti",
-      AUTH_COOKIE_NAME: "epsilon",
-      CLOUD_NAME: "chitti",
-      CLOUD_UPLOAD_PRESET: "chitti_dp",
-    },
-  };
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
+  env: {
+    MONGO_DB: "chitti",
+    AUTH_COOKIE_NAME: "epsilon",
+    CLOUD_NAME: "chitti",
+    CLOUD_UPLOAD_PRESET: "chitti_dp",
+  },
 };
+
+const SentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
