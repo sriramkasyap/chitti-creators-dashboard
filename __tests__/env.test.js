8c51d7c60c9402d61aa307faf0f612ec59c7bfab
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-07-07 12:31:42 +0530

    Testing environment setup

---
+++ __tests__/env.test.js
@@ -0,0 +1,14 @@
+describe("Environment Variables", () => {
+  test("Cookie Name", () => {
+    expect(process.env.AUTH_COOKIE_NAME).toBeDefined();
+  });
+  test("Cookie Password", () => {
+    expect(process.env.APPLICATION_SECRET).toBeDefined();
+  });
+  test("Mongo URL", () => {
+    expect(process.env.MONGO_URL).toBeDefined();
+  });
+  test("Mongo DB", () => {
+    expect(process.env.MONGO_DB).toBeDefined();
+  });
+});
