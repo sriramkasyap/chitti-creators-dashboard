66969ba9812fd0416694c3104094bcfd7d531ea9
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-07-07 17:52:04 +0530

    Login Test cases added

--- pages/api/__tests__/creators.test.js
+++ pages/api/__tests__/creators.test.js
@@ -1,6 +1,7 @@
 import { testApiHandler } from "next-test-api-route-handler";
 import mongoose from "mongoose";
 import * as signup from "../creators/signup";
+import * as login from "../creators/login";
 
 async function removeAllCollections() {
   const collections = Object.keys(mongoose.connection.collections);
@@ -17,7 +18,7 @@ beforeAll(() => {
 });
 
 describe("Creators API", () => {
-  test("Creator Signup - Valid", async () => {
+  test("Creator Signup - Valid Data", async () => {
     const dummyData = {
       fullName: "Tester",
       emailId: "test@test.com",
@@ -183,6 +184,92 @@ describe("Creators API", () => {
     });
   });
 
+  test("Creator Login - Correct Credentials", async () => {
+    const dummyData = {
+      emailId: "test@test.com",
+      password: "test@123",
+    };
+    await testApiHandler({
+      handler: login,
+      test: async ({ fetch }) => {
+        const response = await fetch({
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(dummyData),
+        });
+        // Test Response Status
+        expect(response).toHaveProperty("status");
+        expect(response.status).toEqual(200);
+
+        // Test Response  Headers
+        const { headers } = response;
+        expect(headers).toBeDefined();
+        expect(headers.has("set-cookie")).toEqual(true);
+        expect(headers.get("set-cookie")).toContain(
+          process.env.AUTH_COOKIE_NAME
+        );
+        expect(headers.get("set-cookie")).toContain("HttpOnly");
+
+        // Test Response  Body
+        const resBody = await response.json();
+        expect(resBody.success).toBeDefined();
+        expect(resBody.success).toEqual(true);
+        expect(resBody.creator).toBeDefined();
+        expect(resBody.creator).toMatchObject({
+          _id: expect.any(String),
+          emailId: dummyData.emailId,
+          profile: {
+            fullName: expect.any(String),
+          },
+          plans: expect.any(Array),
+        });
+        expect(resBody.creator.password).toBeUndefined();
+        expect(resBody.creator.plans).toHaveLength(1);
+        expect(resBody.creator.plans[0]).toMatchObject({
+          _id: expect.any(String),
+          planFee: 0,
+          planFeatures: [],
+          planRZPid: null,
+        });
+      },
+    });
+  });
+
+  test("Creator Login - Incorrect Credentials", async () => {
+    const dummyData = {
+      emailId: "test@test.com",
+      password: "test@124", // Wrong Password
+    };
+    await testApiHandler({
+      handler: login,
+      test: async ({ fetch }) => {
+        const response = await fetch({
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(dummyData),
+        });
+        // Test Response Status
+        expect(response).toHaveProperty("status");
+        expect(response.status).toEqual(501);
+
+        // Test Response  Headers
+        const { headers } = response;
+        expect(headers).toBeDefined();
+        expect(headers.has("set-cookie")).toEqual(false);
+
+        // Test Response  Body
+        const resBody = await response.json();
+        expect(resBody.error).toBeDefined();
+        expect(resBody.error).toEqual(true);
+        expect(resBody.message).toBeDefined();
+        expect(resBody.message.toLowerCase()).toContain("incorrect");
+      },
+    });
+  });
 });
 
 afterAll(() => {
