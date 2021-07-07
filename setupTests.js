c3003ea610b0521a234e45cf9b5524449979ffbc
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-07-07 21:51:14 +0530

    refactored test env loadig

--- setupTests.js
+++ setupTests.js
@@ -1,16 +1,6 @@
-process.env.MONGO_URL = "mongodb://localhost:27017";
+/* eslint-disable import/no-extraneous-dependencies */
+import dotenv from "dotenv";
 
-process.env.APPLICATION_SECRET = "q[rgdnv[pqehgvq0erabfvq-wr0[usgdhvqear0sgh";
-
-process.env.SENDGRID_DASHBOARD_KEY =
-  "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";
-
-process.env.VERCEL_URL = "http://localhost:3000";
-
-process.env.MONGO_DB = "chitti-tests";
-
-process.env.AUTH_COOKIE_NAME = "epsilon";
-
-process.env.CLOUD_NAME = "chitti";
-
-process.env.CLOUD_UPLOAD_PRESET = "chitti_dp";
+dotenv.config({
+  path: "./.env.tests",
+});
