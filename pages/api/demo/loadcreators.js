f9553c4f1766f1de5688278219c513f0dc0e610b
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-07-17 09:26:18 +0530

    a trailing bracket removed

--- pages/api/demo/loadcreators.js
+++ pages/api/demo/loadcreators.js
@@ -43,7 +43,7 @@ export default withDB(async (req, res) => {
               user.name.first
             } ${
               user.name.last
-            }}. I work as a tech consultant. I regularly write about Computers, Microprocessors and Software. Do subscribe to my newsletter to read more.
+            }. I work as a tech consultant. I regularly write about Computers, Microprocessors and Software. Do subscribe to my newsletter to read more.
             Peace.`,
             longBio: `
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
